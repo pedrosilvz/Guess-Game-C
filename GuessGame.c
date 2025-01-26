a8779e965e0c2400be58ec6da7f42f3fa4ccc9cf
Pedro Silva <pedropstrabalho@gmail.com>
2025-01-26 20:16:56 -0300

    Update GuessGame.c

--- GuessGame.c
+++ GuessGame.c
@@ -6,7 +6,7 @@
 
 int main() {           
 
-    setlocale(LC_ALL, "Portuguese_Brazil");                     // Define o idioma do programa
+    setlocale(LC_ALL, "Portuguese_Brazil");                     
 
 
         printf("\n================================================================================================================================================================\n");
@@ -42,7 +42,7 @@ int main() {
     int acertou = 0;
 
     int dificuldade;
-    printf("\nSelecione a dificuldade: (1) fácil, (2) médio, (3) difícil.\n\n");
+    printf("\nSelecione a dificuldade: (1) fÃ¡cil, (2) mÃ©dio, (3) difÃ­cil.\n\n");
     printf("\nEscolha: ");
     scanf("%d", &dificuldade);
     printf("\n\n");
@@ -62,43 +62,43 @@ int main() {
     }
 
 
-        for (int i = 1; i <= numerodetentativas; i++) {                                 // Loop infinito
-        printf("\nTentativa número %d de %d\n", tentativas, numerodetentativas);              // Imprime a tentativa atual
-        printf("\nQual é o número secreto?: ");      
-        scanf("%d", &chute);                                             // Lê o número digitado pelo usuário e atribui à variável chute --- &chute é o endereço de memória da variável chute    
+        for (int i = 1; i <= numerodetentativas; i++) {                                
+        printf("\nTentativa nÃºmero %d de %d\n", tentativas, numerodetentativas);              
+        printf("\nQual Ã© o nÃºmero secreto?: ");      
+        scanf("%d", &chute);                                                
         printf("\n");                 
 
         if(chute < 0) {
             printf("\n======================================================================================\n");;
-            printf("\n- Você não pode chutar números negativos! O número de tentativas não será descontado!\n");
+            printf("\n- VocÃª nÃ£o pode chutar nÃºmeros negativos! O nÃºmero de tentativas nÃ£o serÃ¡ descontado!\n");
             printf("\n======================================================================================\n");      
-            continue;  // continue é uma instrução que faz com que o loop pule para a próxima iteração (uma iteração é uma única execução de um loop)   
+            continue;     
         }
 
-        acertou = (chute == numeroSecreto);     // Verifica se o número digitado é igual ao número secreto --- 1 se verdadeiro, 0 se falso
+        acertou = (chute == numeroSecreto);     
         int maior = (chute > numeroSecreto);   
         
         if (acertou) {
-            break;         // Encerra o loop
+            break;        
 
-        } else if (maior) {                                                     // else if é uma condição que é verificada se a condição anterior for falsa
+        } else if (maior) {                                                    
             printf("\n===============================================\n");
             printf("\n- SEU CHUTE FOI: %d\n", chute);
-            printf("\n- Você errou! Tente novamente!\n");
-            printf("\n- Seu chute foi maior que o número secreto\n");
+            printf("\n- VocÃª errou! Tente novamente!\n");
+            printf("\n- Seu chute foi maior que o nÃºmero secreto\n");
             printf("\n===============================================\n");  
             }
          else {
                 printf("\n===============================================\n"); 
                 printf("\n- SEU CHUTE FOI: %d\n", chute);
-                printf("\n- Você errou! Tente novamente!\n");
-                printf("\n- Seu chute foir menor que o número secreto\n");
+                printf("\n- VocÃª errou! Tente novamente!\n");
+                printf("\n- Seu chute foir menor que o nÃºmero secreto\n");
                 printf("\n===============================================\n");  
             }
 
-            tentativas++;        // Incrementa o número de tentativas    
+            tentativas++;        // Incrementa o nÃºmero de tentativas    
 
-            double pontosperdidos = abs(chute - numeroSecreto) / (double)2;            //converte a variável para double   //abs serve para transformar o número em positivo
+            double pontosperdidos = abs(chute - numeroSecreto) / (double)2;            
             pontos = pontos - pontosperdidos;          
 
          }  
@@ -108,14 +108,14 @@ int main() {
         
 
          if(acertou){
-            printf("\n   Parabéns! você acertou o número secreto %d\n", numeroSecreto);
+            printf("\n   ParabÃ©ns! vocÃª acertou o nÃºmero secreto %d\n", numeroSecreto);
             printf("\n===============================================\n");
-            printf("\n- Você acertou em %d tentativa(s)!\n", tentativas);    // Imprime o número de tentativas
+            printf("\n- VocÃª acertou em %d tentativa(s)!\n", tentativas);    
             printf("\n- SCORE: %.1f\n\n", pontos);
             printf("===============================================\n\n");
          } else {
-            printf("\n- Você perdeu! Jogue novamente!\n");
-            printf("\n- O número secreto era: %d\n\n", numeroSecreto);
+            printf("\n- VocÃª perdeu! Jogue novamente!\n");
+            printf("\n- O nÃºmero secreto era: %d\n\n", numeroSecreto);
             printf("===============================================\n\n");
          }
      
